39267d055ceca22c24510ab6353ba167c67aa418
Rustam <azimzadeh.rustam@gmail.com>
2023-08-03 01:22:40 +0400

    Add Ex009

---
+++ Ex009/Program.cs
@@ -0,0 +1,45 @@
+﻿// Найти самый первый индекс элемента в массиве со значением из переменной find
+
+void FillArray(int[] collection){
+    int length = collection.Length;
+    int index = 0;
+    while(index < length){
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    Console.Write("numbers = {");
+    while(position < count){
+        Console.Write($"{col[position]},");
+        position++;
+    }
+    Console.Write("}");
+}
+
+int IndexOf(int[] collection, int find){
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count){
+        if(collection[index] == find){
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] numbers = new int[10];
+int find = 4;
+
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine();
+
+int pos = IndexOf(numbers, find);
+Console.WriteLine(pos);
