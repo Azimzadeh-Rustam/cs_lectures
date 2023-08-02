9dff2cd64595bbe34afc032a26f5d20fe0f5eb2b
Rustam <azimzadeh.rustam@gmail.com>
2023-08-03 01:22:23 +0400

    Add Ex008

---
+++ Ex008/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти максимальное число
+
+int FindMax(int[] collection) {
+    int max = collection[0];
+    int length = collection.Length;
+    int index = 0;
+    while(index < length){
+        if(collection[index] > max){
+            max = collection[index];
+        }
+        index++;
+    }
+    return max;
+}
+
+int[] numbers = {5,45,6,75,7,94,1,13,64};
+
+int max = FindMax(numbers);
+
+Console.WriteLine(max);
